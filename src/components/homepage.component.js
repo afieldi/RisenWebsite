6fd6739e8a5bb5bf36a604fee1b21dc234576f26
tguyer <tguyer516@gmail.com>
2020-07-01 19:55:35 -0400

    Add a few things to the homepage. WIP.

--- src/components/homepage.component.js
+++ src/components/homepage.component.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import DatePicker from 'react-datepicker';
+import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 import "react-datepicker/dist/react-datepicker.css";
 
 export default class HomePage extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            teamname: '',
-            duration: 0,
-            date: new Date(),
-            teams: []
-        };
+        this.state = {};
     }
 
     render() {
         return (
-            <div>
-                <h3>Welcome to Risen Esports!</h3>
-            </div>
+            <section style={ pageStyle }>
+                <Container className="p-3">
+                    <Jumbotron style={ outerJumboStyle }>
+                        <h1 className="header">Welcome to Risen Esports</h1>
+                    </Jumbotron>
+                </Container>
+            </section>
         )
     }
-}
+};
+
+// Page style and background color.
+// Just picking a random height for now.
+var pageStyle = {
+    width: '100%',
+    height: 1200,
+    backgroundColor: `rgb(0, 25, 50)`
+};
+
+// Outermost jumbotron style
+var outerJumboStyle = {
+    backgroundColor: `rgb(123, 131, 148)`
+};
