cf99209d9f15317e6cd9355d5571537555e1e267
tguyer <tguyer516@gmail.com>
2020-07-02 02:23:08 -0400

    Add logo to homepage. Set background image.

--- src/App.js
+++ src/App.js
@@ -9,22 +9,34 @@ import EditGame from "./components/edit-game.component";
 import CreateGame from "./components/create-game.component";
 import CreateTeam from "./components/create-team.component";
 import Test from "./components/test.component";
+import backgroundImage from "./images/backgroundimage2.jpg";
 
 function App() {
   return (
-    <Router>
-      <div className="container">
-        <Navbar />
-        <br/>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/gameslist" exact component={GamesList} />
-        <Route path="/edit/:id" component={EditGame} />
-        <Route path="/creategame" component={CreateGame} />
-        <Route path="/team" component={CreateTeam} />
-        <Route path="/test" component={Test} />
+      <div style={ backgroundImageStyle }>
+        <Router>
+          <div className="container">
+            <Navbar />
+            <br/>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/gameslist" exact component={GamesList} />
+            <Route path="/edit/:id" component={EditGame} />
+            <Route path="/creategame" component={CreateGame} />
+            <Route path="/team" component={CreateTeam} />
+            <Route path="/test" component={Test} />
+          </div>
+        </Router>
       </div>
-    </Router>
   );
 }
+
+var backgroundImageStyle = {
+  width: '100%',
+  height: '1600px',
+  backgroundImage: "url(" + backgroundImage + ")",
+  backgroundPosition: 'center',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat'
+};
 
 export default App;
--- src/components/homepage.component.js
+++ src/components/homepage.component.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import "react-datepicker/dist/react-datepicker.css";
+import risenLogo from "../images/RE_TypeLogo_Shading.png";
+import "../App.css";
 
 export default class HomePage extends Component {
     constructor(props) {
@@ -12,10 +14,11 @@ export default class HomePage extends Component {
 
     render() {
         return (
-            <section style={ pageStyle }>
+            <section>
                 <Container className="p-3">
+                    <img src={ risenLogo } className="risenLogoClass"/>
                     <Jumbotron style={ outerJumboStyle }>
-                        <h1 className="header">Welcome to Risen Esports</h1>
+                        <h1>Welcome to Risen Esports</h1>
                     </Jumbotron>
                 </Container>
             </section>
@@ -23,15 +26,8 @@ export default class HomePage extends Component {
     }
 };
 
-// Page style and background color.
-// Just picking a random height for now.
-var pageStyle = {
-    width: '100%',
-    height: 1200,
-    backgroundColor: `rgb(0, 25, 50)`
-};
-
 // Outermost jumbotron style
 var outerJumboStyle = {
-    backgroundColor: `rgb(123, 131, 148)`
+    backgroundColor: `rgb(0, 25, 50)`,
+    opacity: 0.95
 };
