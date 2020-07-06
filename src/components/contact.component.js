f2bf64d55a7b666a21a4a1295f2d1e542a696443
Arek Fielding <arek.fielding@gmail.com>
2020-07-06 16:17:22 -0400

    added contact data

--- src/components/contact.component.js
+++ src/components/contact.component.js
@@ -1,11 +1,36 @@
 import React, {Component} from 'react';
+import { Container } from 'react-bootstrap';
+
+const staticText = require('../data/text.json'); 
 
 export default class Contact extends Component {
 
     render() {
         return (
             <section>
-                
+                <br></br>
+                <br></br>
+                <br></br>
+                <div className="light-section">
+                    <Container>
+                        <h1 className="center text-dark">Need to Contact Us?</h1>
+                        <hr className="risen-dark"></hr>
+                        <p>{staticText.contact.blurb}</p>
+                        {
+                            staticText.contact.people.map((person) => {
+                                return (
+                                    <div>
+                                        <h3>{person.title}</h3>
+                                        <h5>{person.name}</h5>
+                                        <p>Contact for: {person.responsibilities}</p>
+                                        <p>Discord: {person.discord}</p>
+                                        <hr></hr>
+                                    </div>
+                                )
+                            })
+                        }
+                    </Container>
+                </div>
             </section>
         )
     }
