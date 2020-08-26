45f35998aff2b68a03c25129005b10593666db46
Earleking <arek.fielding@gmail.com>
2020-08-26 00:52:38 -0400

    update now works for icons

--- src/data/update.js
+++ src/data/update.js
@@ -6,18 +6,17 @@ fetch("https://ddragon.leagueoflegends.com/api/versions.json").then(response => 
     response.json().then(data => {
         const version = data[0];
         const champ_url = `http://ddragon.leagueoflegends.com/cdn/${version}/data/en_US/champion.json`;
-
+        console.log("Version: " + version);
         fetch(champ_url).then(response2 => {
             response2.json().then(champData => {
                 fs.writeFileSync("src/data/champions.json", JSON.stringify(champData, undefined, 2));
-
                 let newData = {};
                 for (let key in champData["data"]) {
                     newData[champData["data"][key]["key"]] = key
 
-                    if(!fs.existsSync(`src/images/champions/icons/${key}_0.jpg`)) {
-                        fetch(`http://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${key}.jpg`).then(res => {
-                            const dest = fs.createWriteStream(`src/images/champions/icons/${key}_0.jpg`);
+                    if(!fs.existsSync(`src/images/champions/icons/${key}_0.png`)) {
+                        fetch(`http://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${key}.png`).then(res => {
+                            const dest = fs.createWriteStream(`src/images/champions/icons/${key}_0.png`);
                             res.body.pipe(dest);
                         });
                     }
