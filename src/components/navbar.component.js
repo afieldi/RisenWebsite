de64f3d37be80cb6794a3380d453415beec82381
Earleking <arek.fielding@gmail.com>
2021-01-04 14:50:10 -0500

    change nav to pbdraft as regular draft will no longer be used

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -64,7 +64,7 @@ export default class Navbar extends Component {
                 <Link to="/teams" className="nav-link">Team Stats</Link>
               </li> */}
               <li className="navbar-item ml-auto">
-                <Link to="/drafting" className="nav-link">Drafting</Link>
+                <Link to="/pbdraft" className="nav-link">Drafting</Link>
               </li>
               <li className="navbar-item ml-auto">
                 <Link className="nav-link">Watch (Soon)</Link>
