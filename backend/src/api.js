bc749dd7a97a5b04c1c48f0546371d7e7e33346d
Earleking <arek.fielding@gmail.com>
2021-01-05 13:22:34 -0500

    add custom rate limiting

--- backend/src/api.js
+++ backend/src/api.js
@@ -1,8 +1,9 @@
 // const LeagueJS = require('leaguejs');
 // import { LolApi, Constants } from 'twisted';
 const Twisted = require('twisted');
-
+const fetch = require('node-fetch');
 const process = require('process');
+const e = require('express');
 // const leagueJs = new LeagueJS(process.env.RIOT_API);
 const api = new Twisted.LolApi({
     rateLimitRetry: true,
@@ -10,8 +11,31 @@ const api = new Twisted.LolApi({
     key: process.env.RIOT_API
 });
 
+function makeRequest(url, method, key, body) {
+    return fetch(url, {
+        method: method,
+        headers: {
+            'X-Riot-Token': key,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body)
+    }).then(async response => {
+        if (response.ok) {
+            return response;
+        }
+        else if (response.status === 429) {
+            await setTimeout(() => {
+            }, 2000);
+            return await makeRequest(url, method, key, body)
+        }
+        else {
+            throw new Error(response.text);
+        }
+    });
+}
 
 module.exports = {
     leagueApi: api,
-    constants: Twisted.Constants
+    constants: Twisted.Constants,
+    makeRequest: makeRequest
 }
