3c80c385c548ed966aebbb48e5cf1b8e03f756f7
Arek <arek.fielding@gmail.com>
2021-09-07 16:36:26 -0400

    improved update

--- src/data/update.js
+++ src/data/update.js
@@ -8,29 +8,38 @@ fetch("https://ddragon.leagueoflegends.com/api/versions.json").then(response => 
         const champ_url = `http://ddragon.leagueoflegends.com/cdn/${version}/data/en_US/champion.json`;
         console.log("Version: " + version);
         fetch(champ_url).then(response2 => {
-            response2.json().then(champData => {
+            response2.json().then(async champData => {
                 fs.writeFileSync("src/data/champions.json", JSON.stringify(champData, undefined, 2));
                 let newData = {};
                 for (let key in champData["data"]) {
                     newData[champData["data"][key]["key"]] = key
 
                     if(!fs.existsSync(`src/images/champions/icons/${key}_0.png`)) {
-                        fetch(`http://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${key}.png`).then(res => {
-                            const dest = fs.createWriteStream(`src/images/champions/icons/${key}_0.png`);
-                            res.body.pipe(dest);
-                        });
+                        // fetch(`http://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${key}.png`).then(res => {
+                        //     const dest = fs.createWriteStream(`src/images/champions/icons/${key}_0.png`);
+                        //     res.body.pipe(dest);
+                        // });
+                        let res = await fetch(`http://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${key}.png`);
+                        const dest = fs.createWriteStream(`src/images/champions/icons/${key}_0.png`);
+                        res.body.pipe(dest);
                     }
                     if(!fs.existsSync(`src/images/champions/profile/${key}_0.jpg`)) {
-                        fetch(`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${key}_0.jpg`).then(res => {
-                            const dest = fs.createWriteStream(`src/images/champions/profile/${key}_0.jpg`);
-                            res.body.pipe(dest);
-                        });
+                        // fetch(`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${key}_0.jpg`).then(res => {
+                        //     const dest = fs.createWriteStream(`src/images/champions/profile/${key}_0.jpg`);
+                        //     res.body.pipe(dest);
+                        // });
+                        let res = await fetch(`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${key}_0.jpg`);
+                        const dest = fs.createWriteStream(`src/images/champions/profile/${key}_0.jpg`);
+                        res.body.pipe(dest);
                     }
                     if(!fs.existsSync(`src/images/champions/splash/${key}_0.jpg`)) {
-                        fetch(`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${key}_0.jpg`).then(res => {
-                            const dest = fs.createWriteStream(`src/images/champions/splash/${key}_0.jpg`);
-                            res.body.pipe(dest);
-                        });
+                        // fetch(`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${key}_0.jpg`).then(res => {
+                        //     const dest = fs.createWriteStream(`src/images/champions/splash/${key}_0.jpg`);
+                        //     res.body.pipe(dest);
+                        // });
+                        let res = await fetch(`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${key}_0.jpg`);
+                        const dest = fs.createWriteStream(`src/images/champions/splash/${key}_0.jpg`);
+                        res.body.pipe(dest);
                     }
                 }
                 fs.writeFileSync("src/data/champions_map.json", JSON.stringify(newData, undefined, 2));
@@ -39,15 +48,18 @@ fetch("https://ddragon.leagueoflegends.com/api/versions.json").then(response => 
 
         const item_url = `http://ddragon.leagueoflegends.com/cdn/${version}/data/en_US/item.json`;
         fetch(item_url).then(response3 => {
-            response3.json().then(itemData => {
+            response3.json().then(async itemData => {
                 fs.writeFileSync("src/data/items.json", JSON.stringify(itemData, undefined, 2));
 
                 for (let key in itemData["data"]) {
                     if(!fs.existsSync(`src/images/items/${key}.png`)) {
-                        fetch(`http://ddragon.leagueoflegends.com/cdn/${version}/img/item/${key}.png`).then(res => {
-                            const dest = fs.createWriteStream(`src/images/items/${key}.png`);
-                            res.body.pipe(dest);
-                        });
+                        // fetch(`http://ddragon.leagueoflegends.com/cdn/${version}/img/item/${key}.png`).then(res => {
+                        //     const dest = fs.createWriteStream(`src/images/items/${key}.png`);
+                        //     res.body.pipe(dest);
+                        // });
+                        let res = await fetch(`http://ddragon.leagueoflegends.com/cdn/${version}/img/item/${key}.png`);
+                        const dest = fs.createWriteStream(`src/images/items/${key}.png`);
+                        res.body.pipe(dest);
                     }
                 }
             })
@@ -55,15 +67,18 @@ fetch("https://ddragon.leagueoflegends.com/api/versions.json").then(response => 
 
         const summoner_url = `http://ddragon.leagueoflegends.com/cdn/${version}/data/en_US/summoner.json`;
         fetch(summoner_url).then(response4 => {
-            response4.json().then(summonerData => {
+            response4.json().then(async summonerData => {
                 fs.writeFileSync("src/data/summoner.json", JSON.stringify(summonerData, undefined, 2));
 
                 for (let key in summonerData["data"]) {
                     if(!fs.existsSync(`src/images/summoner/${key}.png`)) {
-                        fetch(`http://ddragon.leagueoflegends.com/cdn/${version}/img/spell/${key}.png`).then(res => {
-                            const dest = fs.createWriteStream(`src/images/summoner/${summonerData["data"][key].key}.png`);
-                            res.body.pipe(dest);
-                        });
+                        // fetch(`http://ddragon.leagueoflegends.com/cdn/${version}/img/spell/${key}.png`).then(res => {
+                        //     const dest = fs.createWriteStream(`src/images/summoner/${summonerData["data"][key].key}.png`);
+                        //     res.body.pipe(dest);
+                        // });
+                        let res = await fetch(`http://ddragon.leagueoflegends.com/cdn/${version}/img/spell/${key}.png`);
+                        const dest = fs.createWriteStream(`src/images/summoner/${summonerData["data"][key].key}.png`);
+                        res.body.pipe(dest);
                     }
                 }
             })
@@ -71,17 +86,20 @@ fetch("https://ddragon.leagueoflegends.com/api/versions.json").then(response => 
 
         const runes_url = `http://ddragon.leagueoflegends.com/cdn/${version}/data/en_US/runesReforged.json`;
         fetch(runes_url).then(response5 => {
-            response5.json().then(runeData => {
+            response5.json().then(async runeData => {
                 fs.writeFileSync("src/data/runes.json", JSON.stringify(runeData, undefined, 2));
 
                 for (let style of runeData) {
                     for (let slot of style.slots) {
                         for (let rune of slot.runes) {
                             if(!fs.existsSync(`src/images/runes/${rune.id}.png`)) {
-                                fetch(` https://ddragon.leagueoflegends.com/cdn/img/${rune.icon}`).then(res => {
-                                    const dest = fs.createWriteStream(`src/images/runes/${rune.id}.png`);
-                                    res.body.pipe(dest);
-                                });
+                                // fetch(`https://ddragon.leagueoflegends.com/cdn/img/${rune.icon}`).then(res => {
+                                //     const dest = fs.createWriteStream(`src/images/runes/${rune.id}.png`);
+                                //     res.body.pipe(dest);
+                                // });
+                                let res = await fetch(`https://ddragon.leagueoflegends.com/cdn/img/${rune.icon}`);
+                                const dest = fs.createWriteStream(`src/images/runes/${rune.id}.png`);
+                                res.body.pipe(dest);
                             }
                         }
                     }
